b38bc951c06d2850b505f600a0aff84a680f446c
reycar192001 <carlosalfredo200119@gmail.com>
2025-01-18 19:17:19 -0500

    primer avance del main y index

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { IndexComponent } from './index/index.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:MainComponent,title: 'Peloteros'},
+    {path:'index',component:IndexComponent},
+];
